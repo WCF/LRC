d930d7a5913b2de45565aebad2eb8c55308c505d
Brian Lai <b2lai@uwaterloo.ca>
2013-01-03 18:52:24 -0500

    optimise vars.php for easy config

--- index.php
+++ index.php
@@ -1,42 +1,59 @@
 <?php require_once('pop/pop.php'); ?>
 
-    <img id="campfire" style="background-image:url('honne-campfire.jpg'); background-position: center center;" width="760" height="280"/>
+<img id="campfire"
+     style="background-image:url('honne-campfire.jpg'); background-position: center center;"
+     width="760" height="280"/>
 
-    <section id="linkList">
-      <ul>
+<section id="linkList">
+    <ul>
         <li>
-          <a href="sermon.html">Sermon</a>
+            <a href="sermon.html">Sermon</a>
         </li>
         <li>
-          <a href="newsletter.html">Newsletter</a>
+            <a href="newsletter.html">Newsletter</a>
         </li>
         <li>
-          <a href="stuff.html">Stuff</a>
+            <a href="stuff.html">Stuff</a>
         </li>
         <li>
-          <a href="otherStuff.html">Stuff</a>
+            <a href="otherStuff.html">Stuff</a>
         </li>
-    </section>
+</section>
 
-    <table id="events">
-      <tr>
-        <td class="date"><time><span class="month">DEC</span><span class="day">24</span></time></td>
+<table id="events">
+    <tr>
+        <td class="date">
+            <time><span class="month">DEC</span><span class="day">24</span>
+            </time>
+        </td>
         <td class="title">Christmas Eve</td>
-      </tr>
-      <tr>
-        <td class="date"><time><span class="month">DEC</span><span class="day">25</span></time></td>
+    </tr>
+    <tr>
+        <td class="date">
+            <time><span class="month">DEC</span><span class="day">25</span>
+            </time>
+        </td>
         <td class="title">Christmas!</td>
-      </tr>
-      <tr>
-        <td class="date"><time><span class="month">DEC</span><span class="day">26</span></time></td>
+    </tr>
+    <tr>
+        <td class="date">
+            <time><span class="month">DEC</span><span class="day">26</span>
+            </time>
+        </td>
         <td class="title">Boxing Day</td>
-      </tr>
-      <tr>
-        <td class="date"><time><span class="month">DEC</span><span class="day">31</span></time></td>
+    </tr>
+    <tr>
+        <td class="date">
+            <time><span class="month">DEC</span><span class="day">31</span>
+            </time>
+        </td>
         <td class="title">New Year's Eve</td>
-      </tr>
-      <tr>
-        <td class="date"><time><span class="month">JAN</span><span class="day">01</span></time></td>
+    </tr>
+    <tr>
+        <td class="date">
+            <time><span class="month">JAN</span><span class="day">01</span>
+            </time>
+        </td>
         <td class="title">New Year's Day</td>
-      </tr>
-    </table>
+    </tr>
+</table>
--- vars.php
+++ vars.php
@@ -19,12 +19,12 @@
         ?>
     */
 
-    // set to false if using POP as persistance library
-    if (!defined ('USE_POP_REDIRECTION')) {
+    // set to false if using POP as persistence library
+    if (!defined('USE_POP_REDIRECTION')) {
         define ('USE_POP_REDIRECTION', false);
     }
 
-    set_time_limit (3); // preferred; prevents DDoS?
+    set_time_limit(3); // preferred; prevents DDoS?
     define ('DOMAIN', 'http://' . $_SERVER['SERVER_NAME']);
     define ('DATA_PATH', PATH . 'data/');
     define ('CACHE_PATH', PATH . 'cache/');
@@ -35,14 +35,14 @@
     define ('STATIC_PATH', 'static/'); // cannot be changed
     define ('SITE_TEMPLATE', 'default.html');
     define ('DEFAULT_TEMPLATE', 'default.html');
-    define ('FS_FETCH_HARD_LIMIT', PHP_INT_MAX); // when should Query give up?
+    define ('FS_FETCH_HARD_LIMIT', 1000); // when should Query give up?
     define ('TEMPLATE_COMPRESS', false); // use compressor = more CPU, less bandwidth
-    define ('SITE_SECRET', 'n00bness'); // for ajax. Change immediately!
+    define ('SITE_SECRET', '12345678'); // for ajax. Change immediately!
     define ('WRITE_ON_MODIFY', true); // if false, Model.put() is required
 
     // SUBDIR: exclude prefix slash, include trailing slash.
     // define ('SUBDIR', substr (PATH, strlen ($_SERVER['DOCUMENT_ROOT'])));
-    define ('SUBDIR', substr (PATH, strlen ($_SERVER['DOCUMENT_ROOT'])));
+    define ('SUBDIR', substr(PATH, strlen($_SERVER['DOCUMENT_ROOT'])));
 
     define ('WIN', 5);
     $win = WIN;
