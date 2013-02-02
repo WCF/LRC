ed38d9db79215c974680ada2246695f155d43aae
Sarah Lannigan <sarah@willetinc.com>
2013-02-02 16:36:11 -0500

    Formatted CR page

--- programs-celebrate-recovery.php
+++ programs-celebrate-recovery.php
@@ -1,3 +1,172 @@
 <?php require_once('pop/pop.php'); ?>
 <h1><a href="programs.php">Programs</a></h1>
-<h2>Celebrate Recovery</h2>
+<h2>Celebrate Recovery&reg;</h2>
+<img src="static/img/titanic-beach.jpg">
+<blockquote>
+"At Celebrate Recovery, God gave me in recovery, peace of mind, freedom from addiction, a new identity in Christ, a support group with caring people, opportunity to share my experience, strength & hope with others who struggle and to experience unconditional acceptance without judgment from fellow Christians."
+</blockquote>
+~A participant of Celebrate Recovery Waterloo
+<br><br>
+
+<p>
+<span class="emph">Celebrate Recovery</span> is a Christ-Centered Recovery Program for all life's hurts, habits & hang ups. 
+</p><p>
+The purpose of the Celebrate Recovery ministry is to fellowship and celebrate God's healing power in
+our lives through the <a href="static/pdf/CR_Eight_Recovery_Principles.pdf">8 Recovery Principles</a>. By working and applying these Biblical principles, we begin to grow spiritually. We become free from our addictive, compulsive and dysfunctional behaviors.
+</p><p>
+This freedom creates peace, <a href="static/pdf/CR_Prayer_For_Serenity.pdf">serenity</a>, joy and most importantly, a stronger personal relationship with God and others.
+</p><p>
+More information can be found on the <a href="http://www.celebraterecovery.ca">Celebrate Recovery website</a>
+</p>
+<h3>Lincoln Road Celebrate Recovery</h3>
+<table>
+    <tr>
+        <td>Day:</td>
+        <td>Every Thursday (does not break for holidays!)</td>
+    </tr>
+    <tr>
+        <td>Time:</td> 
+        <td>7:00 pm - 10:00 pm</td>
+    </tr>
+    <tr>
+        <td>Location:</td> 
+        <td>Lincoln Road Chapel </td>
+    </tr>
+    <tr>
+        <td>Contact:</td>
+        <td>Michelle Gowland, Ministry Leader at 519-747-2211 x 228 or <a href="mailto:michelle@lincolnroadchapel.ca">michelle@lincolnroadchapel.ca</a></td>
+    </tr>
+    <tr>
+        <td>Format:</td>
+        <td>7:00 pm - Worship and Recovery Teaching or Life Change Testimony <br>
+            8:00 pm - Open Share Groups (gender and issue specific)<br>
+            9:00 pm - Cafe</td>
+    </tr>
+    <tr>
+        <td>Open Share Groups: </td>
+        <td>
+            <ul class="CR-group-categories">
+                <li>For new participants: 
+                    <ul class="CR-groups">
+                        <li>CR 101 available every week</li>
+                    </ul>
+                </li> 
+                <li>For Women:
+                    <ul class="CR-groups">
+                        <li>Anger and Losses</li>
+                        <li>Chemical Addiction</li>
+                        <li>Codependency &amp; Boundaries</li>
+                        <li>Codependency &amp; Control</li>
+                        <li>Food Related Issues &amp; Sexual Addiction</li>
+                    </ul>
+                </li>
+               <li>For Men:
+                    <ul class="CR-groups">
+                        <li>Chemical Addiction</li>
+                        <li>Codependency, Hurts and Hang-ups</li>
+                        <li>Sexual Addiction</li>
+                    </ul>
+                </li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td>Step Studies:</td>
+        <td>Email <a href="mailto:michelle@lincolnroadchapel.ca">michelle@lincolnroadchapel.ca</a> for dates, times and locations.</td>
+    </tr>
+</table>
+ 
+<h3>Creekside Celebrate Recovery</h3>
+<table>
+    <tr>
+        <td>Day:</td>
+        <td>Friday</td>
+    </tr>
+    <tr>
+        <td>Time:</td> 
+        <td>7:00 pm - 10:00 pm</td>
+    </tr>
+    <tr>
+        <td>Location:</td> 
+        <td>Creekside Church, Waterloo </td>
+    </tr>
+    <tr>
+        <td>Contact:</td>
+        <td>Deb Jones at <a href="mailto:djones@creeksidechurch.ca">djones@creeksidechurch.ca</a></td>
+    </tr>
+    <tr>
+        <td>Format:</td>
+        <td>6:00 pm - Dinner (all ages welcome)<br>
+            7:00 pm - Adult Large Group<br>
+            8:00 pm - Confidential Open Share Groups<br>
+            7:00 pm - Kids (13 and under): Celebration Station and Nursery<br>
+            7:00 pm - Youth: The Landing<br>
+            9:00 pm - Cafe</td>
+    </tr>
+    <tr>
+        <td>Open Share Groups: </td>
+        <td>
+            <ul class="CR-groups">
+                <li>Hurts, Habits and Hang-ups for women</li>
+                <li>Hurts, Habits and Hang-ups for men</li>
+                <li>Addictions for men</li>
+                <li>Addictions for women</li>
+                <li>Celebrate Recovery 101 for new participants each week</li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td>Step Studies:</td>
+        <td>Email <a href="mailto:michelle@lincolnroadchapel.ca">michelle@lincolnroadchapel.ca</a> for dates, times and locations.</td>
+    </tr>
+</table> 
+<h3>Kitchener Celebrate Recovery</h3>
+<table>
+    <tr>
+        <td>Day:</td>
+        <td>Monday</td>
+    </tr>
+    <tr>
+        <td>Time:</td> 
+        <td>7:30 pm</td>
+    </tr>
+    <tr>
+        <td>Location:</td> 
+        <td><a href="http://www.rayofhope.net/content/community-centre">Ray of Hope Community Centre, 659 King St. E, Kitchener (rear entrance off Stirling Lane)</td>
+    </tr>
+    <tr>
+        <td>Contact:</td>
+        <td>Joe Graham at 519-724-0265 or <a href="mailto:jgraham@creeksidechurch.ca">jgraham@creeksidechurch.ca</a></td>
+    </tr>
+    <tr>
+        <td>Format:</td>
+        <td>6:00 pm - Dinner (all ages welcome)<br>
+            7:00 pm - Adult Large Group<br>
+            8:00 pm - Confidential Open Share Groups<br>
+            7:00 pm - Kids (13 and under): Celebration Station and Nursery<br>
+            7:00 pm - Youth: The Landing<br>
+            9:00 pm - Cafe</td>
+    </tr>
+    <tr>
+        <td>Open Share Groups: </td>
+        <td>
+            <ul class="CR-groups">
+                <li>Hurts, Habits and Hang-ups for women</li>
+                <li>Hurts, Habits and Hang-ups for men</li>
+                <li>Addictions for men</li>
+                <li>Addictions for women</li>
+                <li>Celebrate Recovery 101 for new participants each week</li>
+            </ul>
+        </td>
+    </tr>
+    <tr>
+        <td>Step Studies:</td>
+        <td>Email <a href="mailto:michelle@lincolnroadchapel.ca">michelle@lincolnroadchapel.ca</a> for dates, times and locations.</td>
+    </tr>
+</table> 
+
+<h3>Celebrate Recovery Canada</h3>
+For information about groups in Canada contact Deb at <a href="mailto:deb@celebraterecovery.ca">deb@celebraterecovery.ca</a>
+
+<p>Celebrate Recovery is a registered trademark and was created at Saddleback Church by Pastors
+John Baker & Rick Warren.</p>
