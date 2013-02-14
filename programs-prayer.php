70337526509c9c3c5bc77c09ac2b5e34540ff85b
Sarah Lannigan <sarah@willetinc.com>
2013-02-13 20:00:53 -0500

    Done prayer page

--- programs-prayer.php
+++ programs-prayer.php
@@ -1,3 +1,37 @@
 <?php require_once('pop/pop.php'); ?>
 <h1><a href="programs.php">Programs</a></h1>
 <h2>Prayer</h2>
+
+<h4>Renate Ruppel, Prayer Chain Coordinator</h4>
+<blockquote>
+"Carry each other's burdens, and in this way you will fulfill the law of Christ" (Galatians 6:2). 
+</blockquote>
+<p>
+As believers, prayer is the most powerful tool we have to communicate with God. The prayer chain exists as a way for members of Lincoln Road Chapel to communicate emergency prayer requests. The prayer chain allows us to partner with one another and God to carry each others burdens. We pray for God's will to be done in difficult and trying situations.
+</p>
+
+<b>Phone:</b> 519-747-2211 ext. 221 <br>
+<b>Email:</b> <a href="mailto:info@lincolnroadchapel.ca">info@lincolnroadchapel.ca</a> <br>
+
+<p>
+Lincoln Road Chapel firmly believes that God is the One who changes people's lives.  We want to be a church who consistently goes to God for our all our needs, depending on Him for everything.  We actively support and encourage individual, group and corporate prayer and below are a number of ways how you can become more involved in prayer at Lincoln Road Chapel:
+</p>
+
+<h4>LinkGroups</h4>
+<p>
+<a href="programs-link-groups.php">LinkGroups</a> are wonderful way to be prayed for and to pray for others.  In a LinkGroup, you meet with the same individuals on a weekly or bi-weekly basis, studying the Bible, sharing with and praying for one another.  To find out more information about LinkGroups, visit our LinkGroups page.
+</p>
+
+<h4>Prayer Chain</h4>
+<p>
+Lincoln Road Chapel has a Prayer Chain designated for emergency prayer items.  If you have an item you need prayer for immediately contact the church office.  If you have an emergency prayer request, please tell us today and we will pray for you.  If you would like to be a part of this Prayer Chain, <a href="contact.php">contact the church office</a>.
+</p>
+
+<h4>Bi-Monthly Prayer Meetings</h4>
+<p>
+Every two weeks, at 6:30 pm, we gather together for corporate prayer.  During these meetings we pray for the members of our congregation, our leaders and our community.  To find out when our next Prayer Meeting is, visit our Calendar.
+</p>
+
+<blockquote>
+"Do not be anxious about anything, but in everything, by prayer and petition, with thanksgiving, present your requests to God" (Philippians. 4:6).
+</blockquote>
