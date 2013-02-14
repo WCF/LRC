858c9f408cbd3f75750ebd5ffe3e5280b137ea90
Brian Lai <b2lai@uwaterloo.ca>
2013-02-13 19:16:39 -0500

    formatted students page

--- programs-students.php
+++ programs-students.php
@@ -1,3 +1,40 @@
 <?php require_once('pop/pop.php'); ?>
 <h1><a href="programs.php">Programs</a></h1>
 <h2>Students</h2>
+<img width="150" height="225" border="0" align="left"
+     style="padding: 0 10px 10px 0"
+     title="PJ_Wong_2011.jpg" alt="PJ_Wong_2011.jpg"
+     src="http://www.lincolnroadchapel.ca/images/PJ_Wong_2011.jpg">
+<p><b>
+    PJ Wong, Youth and Young Adult Programs
+</b></p>
+
+<p>
+    "The Student Ministries at Lincoln Road Chapel are all
+    about loving God and loving other people as we try to figure
+    out what life is really about! Whether you are in your preteens
+    at Xtreme Youth or your teenage years at the LINK, these
+    communities of faith would love to have you included!"
+    <br />
+    <br />
+    <a href="mailto:pj%5Bat%5Dlincolnroadchapel%5Bdot%5Dca">pj[at]lincolnroadchapel[dot]ca</a>
+</p>
+<div>
+    <b>Student Ministries</b>
+    at Lincoln Road Chapel includes Xtreme Youth (grades 6-8) and
+    The LINK (grades 9-12). Click logo for more information.
+    <br>
+
+    <div style="text-align:center;">
+        <a href="http://www.lincolnroadchapel.ca/Xtreme_Youth.php">
+            <img width="249" height="186" border="0"
+                 title="xtreme_pic.jpg" alt="xtreme_pic.jpg"
+                 src="http://www.lincolnroadchapel.ca/images/xtreme_pic.jpg">
+        </a>
+        <a href="http://www.lincolnroadchapel.ca/The_LINK.php">
+            <img width="249" height="191" border="0"
+                 title="the_link.jpg" alt="the_link.jpg"
+                 src="http://www.lincolnroadchapel.ca/images/the_link.jpg">
+        </a>
+    </div>
+</div>
