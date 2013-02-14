36ee0732eb7907fef2cd85c72db23b68d069fcd2
Brian Lai <b2lai@uwaterloo.ca>
2013-02-13 19:08:14 -0500

    formatted children page

--- programs-children.php
+++ programs-children.php
@@ -1,3 +1,127 @@
 <?php require_once('pop/pop.php'); ?>
-<h1><a href="programs.php">Programs</a></h1>
+<h1><a href="/programs.php">Programs</a></h1>
 <h2>Children</h2>
+<div>
+    <img src="http://lincolnroadchapel.ca/images/linkin_kids_logo_2010.png"
+         alt="" align="left" border="0" height="103" width="250"
+         style="padding: 0 10px 10px 0;">
+    <b>
+        Did you know that the set of beliefs a person holds when they are
+        13 years old is often what they will die believing? (George Barna).
+        This means that ministering to our children is vital! Among all the
+        Bible stories, memory verses, songs and activities that go into
+        children’s ministry, it is important that when children graduate
+        from our ministry they haven’t missed Jesus at the center of it
+        all. Therefore our main priority is introducing kids to Jesus and
+        our prayer and desire is that their hearts are captured by God’s
+        love and grace demonstrated at the cross. Further, we desire that
+        kids would learn how to be close followers of Jesus and grow into
+        mature, multiplying disciples"
+    </b>
+</div>
+<table border="0" width="100%">
+    <tbody>
+    <tr>
+        <td>
+            <b>
+                <img style="float: left; border: 0px none; padding: 10px;"
+                     src="http://lincolnroadchapel.ca/images/Nicole_Fisher.jpg"
+                     alt="Nicole_Fisher.jpg" title="Nicole_Fisher.jpg"
+                     height="124" width="100">
+            </b>
+            <br/>
+            <b>
+                Contact info: Nicole Fisher
+            </b>
+            <br/>
+            <b>
+                Volunteer Security Officer
+            </b>
+            <br/>
+            Email:
+            <a href="mailto:nicole[at]lincolnroadchapel[dot]ca">nicole[at]lincolnroadchapel[dot]ca</a>
+        </td>
+        <td>
+            <b>
+                TEACHER'S INFORMATION:
+            </b>
+            <br/>
+            <a href="pgm-download_media.php?name=Jan_to_Apr_2013-_9am.final.pdf">
+                9am Volunteer Schedule
+            </a>
+            <br/>
+            <a href="pgm-download_media.php?name=2013Jan-June_11am.final.2.pdf">
+                11 am
+                Volunteer Scheule
+            </a>
+            <br/>
+            <a href="/pgm-download_media.php?name=LK_registration_form_March11_2012.pdf">Child
+                Registration Form</a>
+            <br/>
+            <a href="pgm-download_media.php?name=LK_ConsentDisclaimer2013.pdf">
+                <span>Consent Disclaimer</span>
+            </a>
+        </td>
+    </tr>
+    </tbody>
+</table>
+<div>
+    <p>
+        <i>Linkin’ Kids</i>
+        is the children’s ministry of Lincoln Road Chapel. The goal of Linkin’
+        Kids is to connect the children of Lincoln Road Chapel to God and to
+        their peers and adults in the family of God. We aim to be
+        Christ-centered in all we do, teach the Bible faithfully and
+        creatively, foster growing relationships, encourage Holy Spirit
+        transformation and mobilize children to serve
+        .
+    </p>
+
+    <p>
+        The main programming is on Sunday mornings but there are also special
+        events monthly and seasonal programs. Linkin’ Kids includes
+        <a href="/index.php?pr=Nursery">
+            nursery
+        </a>
+        (0-23 months),
+        <a href="/index.php?pr=Preschool">
+            preschool
+        </a>
+        (2 year olds ~ SK), and
+        <a href="/index.php?pr=Elementary">
+            elementary (grade 1 ~ 5) students.
+    </p>
+
+    <p>
+        Seasonal events are provided to give children and parents an
+        opportunity to connect outside of the Sunday service. Events  we have
+        done in the past include Amazing Race Family Edition, Banana Day,
+        Family Winter Celebration, and a Carnival. A week-long Vacation Bible
+        School program is also offered in August for children from Lincoln Road
+        Chapel and the community.  For a list of upcoming events, visit our
+        <a href="/index.php?pr=Upcoming_Events">Upcoming Events</a>
+        page.
+    </p>
+
+    <p>
+        All volunteers in Linkin’ Kids programs are screened through an
+        application, police checks, and reference checks with Nicole, our
+        Volunteer Security Coordinator:
+        <a href="mailto:nicole[at]lincolnroadchapel[dot]ca">nicole[at]lincolnroadchapel[dot]ca</a>
+        . Volunteers are equipped and trained to properly care for children and
+        shepherd them in their walk with God. We currently have need for
+        volunteers in many areas of Children's ministry.
+    </p>
+
+    <p>
+        If you think you can use your gifts to serve in the Children's
+        Ministry, and would like to volunteer,
+        please contact
+        Nicole, Our Volunteer Security Officer:
+        <a href="mailto:nicole[at]lincolnroadchapel[dot]ca">
+            nicole[at]lincolnroadchapel[dot]ca</a>
+        and check the
+        <a href="/opportunities.php">Opportunities</a>
+        page for application forms.
+    </p>
+</div>
