da2f0e73c47b259ccd8f27ab6c7de25e38902eff
Brian Lai <b2lai@uwaterloo.ca>
2013-02-13 19:42:21 -0500

    formatted volunteering page

--- programs-volunteering.php
+++ programs-volunteering.php
@@ -1,3 +1,40 @@
 <?php require_once('pop/pop.php'); ?>
 <h1><a href="programs.php">Programs</a></h1>
 <h2>Volunteering</h2>
+
+<p>
+    If you are a member of Lincoln Road Chapel and would like to volunteer,
+    there are many oppotunities to serve. For more information, check the list
+    below and contact a ministry leader.
+    <br><br>
+    To apply to volunteer, please contact Nicole,our Volunteer Security
+    Coordinator:
+    <a href="mailto:nicole[at]lincolnroadchapel[dot]ca">nicole[at]lincolnroadchapel[dot]ca</a>
+</p>
+
+<h3>Information</h3>
+
+<ul>
+    <li>Audio Visual: contact harry[at]arci[dot]ca</li>
+    <li>Library: contact angelica[dot]pchef[at]gto[dot]net</li>
+    <li>Lincoln Welcome Services: contact
+        brenda[dot]kirkwood[at]gmail]dot]com
+    </li>
+    <li>Linkgroup Leader or Host: contact david[at]lincolnroadchapel[dot]ca
+    </li>
+    <li>Linkin' Kids: contact: nicole[at]lincolnroadchapel[dot]ca</li>
+    <li>Nursery Volunteer: contact nicole[at]lincolnroadchapel[dot]ca</li>
+    <li>Women's Programs: contact debbie[at]lincolnroadchapel[dot]ca</li>
+    <li>Worship Leader or Musician: contact gern[at]rogers[dot]com</li>
+    <li>Youth Helper: contact pj[at]lincolnroadchapel[dot]ca</li>
+</ul>
+
+<h4>Volunteer application forms for working with children and youth</h4>
+<ul>
+    <li>Adult volunteer application (grade nine and above): click <a
+            href="http://www.lincolnroadchapel.ca/pgm-download_media.php?name=Adult_volunteer_application_OCT_2011.pdf">here</a>
+    </li>
+    <li>Youth volunteer application (under grade eight): click <a
+            href="http://www.lincolnroadchapel.ca/media/Volunteer_application_form_2.pdf">here</a>
+    </li>
+</ul>
