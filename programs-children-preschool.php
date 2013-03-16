830981bcedbc90ec876ce196d543e5599b4e4b4e
Tom Gault <wartthog@gmail.com>
2013-03-16 14:02:26 -0400

    Added preschool page.

---
+++ programs-children-preschool.php
@@ -0,0 +1,85 @@
+<?php require_once('pop/pop.php'); ?>
+<h1><a href="programs.php">Programs</a></h1>
+<h2>Children - Preschool Program</h2>
+
+<img src="/static/img/preschool-kid.jpg" class="float-right">
+<blockquote>"I learn about being special and kind in Sunday School and I like to colour and play with the dolls."<br>
+    ~Preschool Student</blockquote>
+
+<p>
+    <table>
+        <tr>
+            <td>Date:</td>
+            <td>Sundays</td>
+        </tr>
+        <tr>
+            <td>Time:</td>
+            <td>9:00 am - 10:25 am and 11:00 am - 12:25 am</td>
+        </tr>
+        <tr>
+            <td>Location:</td>
+            <td>
+                Lincoln Road Chapel, Owl Daycare Building
+                <ul>
+                    <li>2 &amp; 3 Year Olds - Preschool Room 1</li>
+                    <li>JK (4 &amp; 5 Year Olds) - Preschool Room 2</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    Note: Children move to the next age level in the months of January, May and September.
+</p>
+
+<h3>Curriculum</h3>
+<p>Our curriculum, "In the Beginning" will help your child grow more aware of God's love, care and help in their
+    everyday lives.  Your child will actively explore Bible truths and apply them to their life through creative large
+    group Bible teaching and small group shepherding. Your child will engage in a variety of activities including
+    listening to the Bible story, singing worship songs, having snack, doing crafts, playing games and activities and
+    colouring.</p>
+
+<h3>Number System</h3>
+<p>When you drop your child off at the nursery,  your child will be assigned a number.  This number is listed beside
+    your child's name on the attendance sheet.  If you are needed during the service, your number will be projected in
+    the sanctuary.  If you see your number, please make your way to their class immediately as your child needs your
+    assistance!</p>
+
+<h3>Volunteers</h3>
+<p>Each class is staffed by a lead shepherd and two volunteers from the congregation. All volunteers have completed
+    police checks and are trained in our child abuse policy.   When you are interested in volunteering, talk to Nicole
+    at <a href="mailto:nicole@lincolnroadchapel.ca">nicole@lincolnroadchapel.ca</a> or 519-747-2211 ext 235..</p>
+
+<h3>Snack</h3>
+<p>A snack will be provided for your child. It usually consists of arrowroot or graham cookies or whole grain crackers
+    and apple juice. Please make the volunteers are aware of any allergies or food intolerances.</p>
+
+<h3>Belongings</h3>
+<p>When dropping off your child for a morning in our children's program, please bring a diaper bag with some diapers in
+    case your child needs changing (if not potty-trained).  Also, please label all belongings with your child's name so
+    we can ensure that they are returned to you.  This includes all diaper bags, cups, toys, etc.</p>
+
+<h3>Drop Off &amp; Pick Up</h3>
+<p>Please sign your initials when you drop  your child off.  If it is your first time attending the preschool, please
+    fill out a registration form so that volunteers have the necessary information about your child.  We also ask that
+    the same parent who dropped off your child pick them up.  This is for the safety of your children.  Do not forget to
+    sign your child out so we know they are safely in your care.
+</p>
+
+<h3>Wellness Policy</h3>
+<p>We seek to provide a healthy environment for your children and ask that you do not bring your child to Sunday School
+    if you observe any of the following:
+    <ul>
+        <li>Fever</li>
+        <li>Discharge in or around the eyes</li>
+        <li>Excessive discharge from the nose</li>
+        <li>Excessive coughing</li>
+        <li>Diarrhea</li>
+        <li>Rash (other than diaper rash)</li>
+        <li>Known communicable disease</li>
+    </ul>
+</p>
+
+<p>Our volunteers follow the same guidelines.  If your child becomes ill while in the nursery, you will be notified
+    immediately.</p>
+
+<p>For more information, contact <a href="mailto:nicole@lincolnroadchapel.ca">Nicole</a>.</p>
+
