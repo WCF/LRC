75448b3aae1d104c019cb5ffcf0bcb1464da8965
Tom Gault <wartthog@gmail.com>
2013-03-16 14:01:50 -0400

    Added nursery page.

---
+++ programs-children-nursery.php
@@ -0,0 +1,72 @@
+<?php require_once('pop/pop.php'); ?>
+<h1><a href="programs.php">Programs</a></h1>
+<h2>Children - Nursery Program</h2>
+
+<img src="/static/img/nusery-kid.jpg" class="float-right">
+<p>The nursery at Lincoln Road Chapel is designed for newborns and toddlers.  It is a safe and loving environment.  We
+    provide the nursery so parents can attend our worship service while knowing that their child is receiving the best
+    care possible.</p>
+
+<p>
+    <table>
+        <tr>
+            <td>Date:</td>
+            <td>Sundays</td>
+        </tr>
+        <tr>
+            <td>Time:</td>
+            <td>9:00 am - 10:25 am and 11:00 am - 12:25 am</td>
+        </tr>
+        <tr>
+            <td>Location:</td>
+            <td>Lincoln Road Chapel, Nursery</td>
+        </tr>
+    </table>
+    Note: Children move to the next age level in the months of January, May and September.
+</p>
+
+<h3>Number System</h3>
+<p>When you drop your child off at the nursery, your child will be assigned a number.  This number is listed beside your
+    child's name on the attendance sheet.  If you are needed during the service, your number will be projected in the
+    sanctuary.  If you see your number, please make your way to the nursery immediately as your child needs your
+    assistance!</p>
+
+<h3>Volunteers</h3>
+<p>The nursery is staffed by two or three scheduled volunteers from the congregation. All volunteers have completed
+    police checks and are trained in our child abuse policy.  When interested in volunteering, talk to Emily Adams, the
+    Nursery Coordinator: <a href="mailto:emilykadams@hotmail.com">emilykadams@hotmail.com</a>.</p>
+
+<h3>Snack</h3>
+<p>Due to allergies, we ask that you provide a snack and drink for your child and the volunteers will distribute it
+    during the service.  If your child has allergies, please inform the volunteers and also make a note of it on the
+    attendance sheet.  Safety is our primary concern!</p>
+
+<h3>Belongings</h3>
+<p>When dropping off your child for a morning in our children's program, please bring a diaper bag with some diapers in
+    case your child needs changing.  Also, please label all belongings with your child's name so we can ensure that they
+    are returned to you.  This includes all diaper bags, cups, toys, etc.</p>
+
+<h3>Drop Off &amp; Pick Up</h3>
+<p>Please sign your initials when you drop your child off.  If it is your first time using the nursery, please fill out
+    a registration form so that volunteers have the necessary information about your child.  We also ask that the same
+    parent who dropped off your child pick them up.  This is for the safety of your children.  Do not forget to sign
+    your child out so we know they are safely in your care.</p>
+
+<h3>Wellness Policy</h3>
+<p>We seek to provide a healthy environment for your children and ask that you do not bring your child to the nursery if
+    you observe any of the following:
+    <ul>
+        <li>Fever</li>
+        <li>Discharge in or around the eyes</li>
+        <li>Excessive discharge from the nose</li>
+        <li>Excessive coughing</li>
+        <li>Diarrhea</li>
+        <li>Rash (other than diaper rash)</li>
+        <li>Known communicable disease</li>
+    </ul>
+</p>
+
+<p>Our volunteers follow the same guidelines.  If your child becomes ill while in the nursery, you will be notified
+    immediately.  All toys in both nurseries are cleaned weekly.</p>
+
+<p>For more information, contact <a href="mailto:nicole@lincolnroadchapel.ca">Nicole</a>.</p>
