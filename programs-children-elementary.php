4933e8fc26992d41ee7c9095d369e49c71d12e0a
Tom Gault <wartthog@gmail.com>
2013-03-16 14:02:48 -0400

    Added elementary page.

---
+++ programs-children-elementary.php
@@ -0,0 +1,85 @@
+<?php require_once('pop/pop.php'); ?>
+<h1><a href="programs.php">Programs</a></h1>
+<h2>Children - Elementary Program</h2>
+
+<img src="/static/img/elementary-kid.jpg" class="float-right">
+<blockquote>"I like Sunday School because I like watching movies about Jesus and seeing my friends."<br>
+    ~Grade 4 Student</blockquote>
+
+<p>
+    <table>
+        <tr>
+            <td>Date:</td>
+            <td>Sundays</td>
+        </tr>
+        <tr>
+            <td>Time:</td>
+            <td>9:00 am - 10:25 am and 11:00 am - 12:25 am</td>
+        </tr>
+        <tr>
+            <td>Location:</td>
+            <td>
+                Lincoln Road Chapel
+                <ul>
+                    <li>Drop-off - Registration Desk at the Visitor Centre</li>
+                    <li>9 am
+                        <ul>
+                            <li>SK - Grade 1 - Room 211</li>
+                            <li>Grade 2 &amp; 3 - Room 215</li>
+                            <li>Grade 4 &amp; 5 - Room 261</li>
+                        </ul>
+                    </li>
+                    <li>11 am
+                        <ul>
+                            <li>SK - Grade 1 - Room 211</li>
+                            <li>Grade 2 &amp; 3 Boys - Room 215</li>
+                            <li>Grade 1 &amp; 4 Girls - Room 261</li>
+                            <li>Digging Deeper (Grade 4 &amp; 5) - Room 214</li>
+                        </ul>
+                    </li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+    Note: Children move to the next age level in the months of January, May and September.
+</p>
+
+<h3>Curriculum</h3>
+<p>Our new curriculum, 252 Basics, is action-packed and involves a high-energy large group worship and teaching time
+    called Power UP! as well as intentional small group shepherding.</p>
+<p>The lessons are built around three Basic Truths every child should embrace according to what Jesus modeled in
+    Luke 2:52.
+    <ul>
+        <li>I need to make the wise choice.</li>
+        <li>I can trust God no matter what.</li>
+        <li>I should treat others the way I want to be treated.</li>
+    </ul>
+    Every week children are reminded of these Basic Truths and learn a new key point derived from the Bible lesson.
+    Every month we focus on a different Christ-like virtue and we work on a memory verse for the entire month that is
+    related to that months virtue.</p>
+
+<h3>Volunteers</h3>
+<p>Each grade level is shepherded by a volunteer from our congregation.  Through teaching, praying, befriending and
+    giving, the shepherd encourages and challenges the child to link with God and the people around them.  Shepherd also
+    build close relationships with the child and their family.  All volunteers have completed police checks and are
+    trained in our child abuse policy</p>
+
+<h3>Bringing It Home</h3>
+<p>Each week children receive fridge door sheets that provide you with information about what your child is learning,
+    announcements about upcoming events, and discussion-creating questions.  Please use these to connect with your child
+    and help them apply their learning to their lives.</p>
+<p>Remember, as a parent you have the most spiritual influence on your child.</p>
+<p>God-Time Cards are also sent home every week with your child.  These contain 4 devotions related to what they learned
+    on Sunday.  Read and talk through these devotions a day at a time.  There are only 4, so you get 3 days off!</p>
+
+<h3>Possible Special Events</h3>
+<ul>
+    <li>Vacation Bible School</li>
+    <li>Halloween Alternative</li>
+    <li>Winter Skating Party</li>
+    <li>March Break day</li>
+    <li>Summer Picnic</li>
+    <li>Christmas, Easter &amp; Mother's Day Performances</li>
+</ul>
+
+<p>For more information, contact <a href="mailto:nicole@lincolnroadchapel.ca">Nicole</a>.</p>
