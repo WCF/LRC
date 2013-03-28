4b77a51a120d704cf2339e61f02bc3b27b94be10
Tom Gault <wartthog@gmail.com>
2013-03-27 20:56:02 -0400

    Whatever version of PHP Sarah's running doesn't like ' = [];'.

--- generate-toc.php
+++ generate-toc.php
@@ -3,7 +3,7 @@
     {
         // This array will be filled with the items of the table of contents.  The key will be the filename to link to and
         // the value will be the title to display.
-        $pages = [];
+        $pages = array();
 
         // Scan through the directory looking for files whose names start with $filenamePrefix
         if ($dirhandle = opendir('.')) {
@@ -16,7 +16,7 @@
                         while (($buffer = fgets($filehandle, 256)) !== false) {
 
                             // Search for the first <h2> tag.  This will be (better be) our title.
-                            $matches = [];
+                            $matches = array();
                             if (preg_match('/<h2>(.*)<\/h2>/', $buffer, $matches)) {
 
                                 // Add the page and bail.
