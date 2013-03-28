ff2f3c5f0c6bb067eb74a8ae3e1df5a4883b4245
Tom Gault <wartthog@gmail.com>
2013-03-27 20:30:44 -0400

    Adding auto-generated table of contents to about pages.

--- about-values.php
+++ about-values.php
@@ -1,30 +1,35 @@
 <?php require_once('pop/pop.php'); ?>
-<h1><a href="about.php">About</a></h1>
-<h2>Values</h2>
 
-<h4>Our Mission</h4>
-<p>
-As a community of Christ followers, Lincoln exists to love God, love people, strengthen the church, serve the world and make disciples who will continue the mission.
-</p>
+<?php include 'output-about-toc.php'; ?>
+<div class="beside-toc">
+    <h1><a href="about.php">About</a></h1>
+    <h2>Values</h2>
 
-<h4>Theme Goal</h4>
-<p>
-To be a Jesus-shaped community bring Jesus to a community.
-</p>
+    <h4>Our Mission</h4>
+    <p>
+    As a community of Christ followers, Lincoln exists to love God, love people, strengthen the church, serve the world and make disciples who will continue the mission.
+    </p>
 
-<h4>Our Key Value</h4>
-<p>
-A place where people matter and Jesus is the main attraction!
-</p>
+    <h4>Theme Goal</h4>
+    <p>
+    To be a Jesus-shaped community bring Jesus to a community.
+    </p>
 
-<h4>Core Values</h4> <br>
-<b>Spiritual Transformation:</b> not conformed to this world but fully alive in Christ <br>
-<b>Commitment to Biblical Base:</b>  God's word is our guide and authority <br>
-<b>Compassionate Service:</b> motivated by genuine care <br>
-<b>Wholehearted Worship:</b> driven by deep gratitude <br>
-<b>Creative Evangelism:</b> sharing the good news relevantly <br>
-<b>Intentional Missions:</b> limited focus for concentrated impact <br>
-<b>Prepared Disciples:</b> biblically literate and trained <br>
-<b>Unified Community:</b> one mind and body in Christ <br>
-<b>Christ Followers:</b> desiring to be like Jesus in word and deed <br>
-<b>Power of Prayer:</b> dependent on and confident of God's faithfulness <br>
+    <h4>Our Key Value</h4>
+    <p>
+    A place where people matter and Jesus is the main attraction!
+    </p>
+
+    <h4>Core Values</h4> <br>
+    <b>Spiritual Transformation:</b> not conformed to this world but fully alive in Christ <br>
+    <b>Commitment to Biblical Base:</b>  God's word is our guide and authority <br>
+    <b>Compassionate Service:</b> motivated by genuine care <br>
+    <b>Wholehearted Worship:</b> driven by deep gratitude <br>
+    <b>Creative Evangelism:</b> sharing the good news relevantly <br>
+    <b>Intentional Missions:</b> limited focus for concentrated impact <br>
+    <b>Prepared Disciples:</b> biblically literate and trained <br>
+    <b>Unified Community:</b> one mind and body in Christ <br>
+    <b>Christ Followers:</b> desiring to be like Jesus in word and deed <br>
+    <b>Power of Prayer:</b> dependent on and confident of God's faithfulness <br>
+</div>
+<div class="bottom-of-contents"></div>
