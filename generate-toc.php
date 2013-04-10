be9de422227dabf16dbb55ddcb06fbc58359f11a
Tom Gault <wartthog@gmail.com>
2013-04-10 17:29:40 -0400

    Making prefix and title regexes case insensitive.

--- generate-toc.php
+++ generate-toc.php
@@ -8,7 +8,7 @@
         // Scan through the directory looking for files whose names start with $filenamePrefix
         if ($dirhandle = opendir('.')) {
             while (($file = readdir($dirhandle)) !== false) {
-                if (preg_match("/^$filenamePrefix/", $file)) {
+                if (preg_match("/^$filenamePrefix/i", $file)) {
 
                     // Scan through the file one line at a time.
                     $filehandle = @fopen($file, "r");
@@ -17,7 +17,7 @@
 
                             // Search for the first <h2> tag.  This will be (better be) our title.
                             $matches = array();
-                            if (preg_match('/<h2>(.*)<\/h2>/', $buffer, $matches)) {
+                            if (preg_match('/<h2>(.*)<\/h2>/i', $buffer, $matches)) {
 
                                 // Add the page and bail.
                                 $pages[$file] = $matches[1];
